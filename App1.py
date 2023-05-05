e703a930af37868ce216ddb109b360df6e634a6e
ernesto <ernestomdelc4@gmail.com>
2023-05-04 23:33:39 -0600

    No meaningful changes

--- App1.py
+++ App1.py
@@ -13,11 +13,15 @@ class MainWindow(QMainWindow): # QMainWindow
         label= QLabel('Primer App') # Widget
         label.setAlignment(Qt.AlignCenter)
         
+        self.setMinimumSize(400,300)
+        self.setMaximumSize(1200,900)
+        # self.setFixedSize(QSize(400, 300)) # Fix window size to 400x300 pixels
+        
         self.setCentralWidget(label)
 
-app= QApplication(sys.argv) # QApplication
+app= QApplication(sys.argv) # QApplication, this is the "Event Handler", we only need one per App
 window = MainWindow()
 
 window.show()
 
-app.exec_() # Event Loop
+app.exec_() # This executes the app when we run the program
